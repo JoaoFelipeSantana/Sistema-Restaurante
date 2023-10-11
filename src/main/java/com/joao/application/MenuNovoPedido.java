73ec899917029686a9a6b66a14bb12e14d48b528
JoaoFelipeSantana <j155c137@gmail.com>
2023-10-10 22:41:33 -0300

    [feat] - Criando um menu para adicionar um novo pedido ao arquivo json

---
+++ src/main/java/com/joao/application/MenuNovoPedido.java
@@ -0,0 +1,31 @@
+package com.joao.application;
+
+import com.joao.domain.FuncoesPedidos;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MenuNovoPedido {
+
+    int id_table, order;
+
+    Scanner scannerPedido = new Scanner(System.in);
+    FuncoesPedidos fPedidos = new FuncoesPedidos();
+
+
+    public void novoPedido() throws IOException {
+
+        System.out.println("\n===== FAÇA SEU NOVO PEDIDO =====");
+
+        System.out.print(" - Informe o número da sua mesa: ");
+        id_table = scannerPedido.nextInt();
+
+        System.out.print(" - Qual opção do cardápio deseja? ");
+        order = scannerPedido.nextInt();
+
+        fPedidos.criarPedido(id_table, order);
+
+        System.out.println("\n\u001B[32m!!! Pedido realizado com SUCESSO !!!\u001B[m");
+
+    }
+}
