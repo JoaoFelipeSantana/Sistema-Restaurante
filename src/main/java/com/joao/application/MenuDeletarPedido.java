c17ce9d229b8a47ffbd54571a230dad1a8466a00
JoaoFelipeSantana <j155c137@gmail.com>
2023-10-13 16:03:38 -0300

    [feat] - Validando se a entrada é um número inteiro

--- src/main/java/com/joao/application/MenuDeletarPedido.java
+++ src/main/java/com/joao/application/MenuDeletarPedido.java
@@ -2,21 +2,38 @@ package com.joao.application;
 
 import com.joao.domain.FuncoesPedidos;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MenuDeletarPedido {
 
     public void deletarPedido() {
-        Scanner scanner = new Scanner(System.in);
         FuncoesPedidos fPedidos = new FuncoesPedidos();
 
         System.out.println("\n===== EXCLUINDO PEDIDO =====");
-        System.out.print(" - Informe o número da mesa: ");
-        int id_table = scanner.nextInt();
+        int flag = 1;
+        while (flag == 1) {
+            Scanner scanner = new Scanner(System.in);
 
-        fPedidos.excluirPedido(id_table);
+            try {
+                System.out.print(" - Informe o número da mesa: ");
+                int id_table = scanner.nextInt();
+                flag = 2;
 
-        System.out.println("\n\u001B[32m !!! PEDIDO EXCLUIDO COM SUCESSO !!! \u001B[m");
+                fPedidos.excluirPedido(id_table);
+
+                System.out.println("\n\u001B[32m !!! PEDIDO EXCLUIDO COM SUCESSO !!! \u001B[m");
+            }
+            catch (InputMismatchException inputMismatchException) {
+                System.out.println("-------------------------------------------------------");
+                System.out.println("\u001B[31m                     !!! ERRO !!!  \u001B[m");
+                System.out.println("Valor digitado fora dos parâmetros de um número inteiro");
+                System.out.println("           Tente novamente (ex: 1, 2, 3...)");
+                System.out.println("-------------------------------------------------------\n");
+
+            }
+
+        }
 
     }
 }
