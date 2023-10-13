d65f3e894d2fc309dc24b8604d80db5a4d7d6e0b
JoaoFelipeSantana <j155c137@gmail.com>
2023-10-13 16:22:41 -0300

    [feat] - Validando a entrada do id como número inteiro

--- src/main/java/com/joao/application/MenuDeleteProduto.java
+++ src/main/java/com/joao/application/MenuDeleteProduto.java
@@ -2,22 +2,36 @@ package com.joao.application;
 
 import com.joao.domain.FuncoesProdutos;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MenuDeleteProduto {
 
     public void excluirProduto() {
-        Scanner scanner = new Scanner(System.in);
         FuncoesProdutos fProdutos = new FuncoesProdutos();
 
         System.out.println("\n===== EXCLUINDO PRODUTO =====");
-        System.out.print(" - Digite id do produto: ");
 
-        int id = scanner.nextInt();
-        fProdutos.delete(id);
+        int flag = 1;
+        while (flag == 1) {
+            Scanner scanner = new Scanner(System.in);
 
-        System.out.println("\n\u001B[32m !!! PRODUTO EXCLUIDO COM SUCESSO !!! \u001B[m");
+            try {
+                System.out.print(" - Digite id do produto: ");
+                int id = scanner.nextInt();
+                flag = 2;
 
-        scanner.close();
+                fProdutos.delete(id);
+                System.out.println("\n\u001B[32m !!! PRODUTO EXCLUIDO COM SUCESSO !!! \u001B[m");
+            }
+            catch (InputMismatchException inputMismatchException) {
+                System.out.println("-------------------------------------------------------");
+                System.out.println("\u001B[31m                     !!! ERRO !!!  \u001B[m");
+                System.out.println("Valor digitado fora dos parâmetros de um número inteiro");
+                System.out.println("           Tente novamente (ex: 1, 2, 3...)");
+                System.out.println("-------------------------------------------------------\n");
+
+            }
+        }
     }
 }
