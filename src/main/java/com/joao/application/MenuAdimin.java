f73b477c9915d65474ac411472024cf5ae7e8e99
JoaoFelipeSantana <j155c137@gmail.com>
2023-10-17 17:07:15 -0300

    [feat] - Adicionando três funções referentes aos pedidos para o usuário administrador

--- src/main/java/com/joao/application/MenuAdimin.java
+++ src/main/java/com/joao/application/MenuAdimin.java
@@ -18,13 +18,16 @@ public class MenuAdimin {
                     "\n [2] Editar produto" +
                     "\n [3] Ver produtos" +
                     "\n [4] Excluir produto" +
+                    "\n [5] Registrar nova mesa" +
+                    "\n [6] Excluir mesa" +
+                    "\n [7] Lista de pedidos" +
                     "\n [0] Voltar a o menu anterior" +
                     "\n==========================");
             System.out.print("Digite a opção que deseja: ");
 
             String opcao = scanner.next();
 
-            if (!opcao.equals("1") && !opcao.equals("2") && !opcao.equals("3") && !opcao.equals("4") && !opcao.equals("0")) {
+            if (!opcao.equals("1") && !opcao.equals("2") && !opcao.equals("3") && !opcao.equals("4") && !opcao.equals("5") && !opcao.equals("6") &&  !opcao.equals("7") && !opcao.equals("0")) {
                 System.out.println("\n\u001B[31m   !!! ERRO !!!   \u001B[m");
                 System.out.println("Digite novamente uma opção válida");
             }
