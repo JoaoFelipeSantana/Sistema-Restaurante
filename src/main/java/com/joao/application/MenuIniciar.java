c1761ae2a8e85b98e50fff343656ca2f08c3011d
JoaoFelipeSantana <j155c137@gmail.com>
2023-10-17 14:34:07 -0300

    [feat] - Adicionando a validação da escolha de usuário e mensagens de erro na application

--- src/main/java/com/joao/application/MenuIniciar.java
+++ src/main/java/com/joao/application/MenuIniciar.java
@@ -9,15 +9,23 @@ public class MenuIniciar {
     ValidacaoEscolhaMenu validar = new ValidacaoEscolhaMenu();
     public void menuUsuario() throws IOException {
         Scanner scanner = new Scanner(System.in);
+        String opcao;
 
-        System.out.println("\n===== USUÁRIO =====");
-        System.out.println(" [1] Administrador" +
-                "\n [2] Cliente" +
-                "\n===================");
-        System.out.print("Digite a opção que deseja: ");
+        while (true) {
+            System.out.println("\n===== USUÁRIO =====");
+            System.out.println(" [1] Administrador" +
+                    "\n [2] Cliente" +
+                    "\n===================");
+            System.out.print("Digite a opção que deseja: ");
+            opcao = scanner.next();
 
-        String opcao = scanner.next();
-
-        validar.validarUsuario(opcao.strip());
+            if (opcao.equals("1") || opcao.equals("2")) {
+                validar.validarUsuario(opcao.strip());
+            }
+            else {
+                System.out.println("\n\u001B[31m   !!! ERRO !!!   \u001B[m");
+                System.out.println("Digite novamente uma opção válida");
+            }
+        }
     }
 }
