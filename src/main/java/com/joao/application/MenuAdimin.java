d1e89eb975612317ceab49726e9f52d2a9f02f5a
JoaoFelipeSantana <j155c137@gmail.com>
2023-10-17 14:50:05 -0300

    [feat] - Validando e mostrando mensagens de erro na seleção da função no menu de Administrador

--- src/main/java/com/joao/application/MenuAdimin.java
+++ src/main/java/com/joao/application/MenuAdimin.java
@@ -8,19 +8,31 @@ import java.util.Scanner;
 public class MenuAdimin {
     ValidacaoEscolhaMenu validar = new ValidacaoEscolhaMenu();
     public void menuProduto() throws IOException {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("\n===== ADIMINISTRADOR =====");
-        System.out.println(" [1] Registrar novo produto" +
-                "\n [2] Editar produto" +
-                "\n [3] Ver produtos" +
-                "\n [4] Excluir produto" +
-                "\n [0] Voltar a o menu anterior" +
-                "\n==========================");
-        System.out.print("Digite a opção que deseja: ");
 
-        String opcao = scanner.next();
-        validar.validarAdimin(opcao.strip());
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
 
+            System.out.println("\n===== ADIMINISTRADOR =====");
+            System.out.println(" [1] Registrar novo produto" +
+                    "\n [2] Editar produto" +
+                    "\n [3] Ver produtos" +
+                    "\n [4] Excluir produto" +
+                    "\n [0] Voltar a o menu anterior" +
+                    "\n==========================");
+            System.out.print("Digite a opção que deseja: ");
+
+            String opcao = scanner.next();
+
+            if (!opcao.equals("1") && !opcao.equals("2") && !opcao.equals("3") && !opcao.equals("4") && !opcao.equals("0")) {
+                System.out.println("\n\u001B[31m   !!! ERRO !!!   \u001B[m");
+                System.out.println("Digite novamente uma opção válida");
+            }
+
+            else {
+                validar.validarAdimin(opcao.strip());
+                break;
+            }
+        }
     }
 }
