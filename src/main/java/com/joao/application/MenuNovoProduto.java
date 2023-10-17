9a39837ba00acf63c8faac7445ec32a390d81062
JoaoFelipeSantana <j155c137@gmail.com>
2023-10-16 21:46:41 -0300

    [feat] - Adicionando mensagens de erro caso o usuário digite valores fora dos parâmetros de um número real ou inteiro

--- src/main/java/com/joao/application/MenuNovoProduto.java
+++ src/main/java/com/joao/application/MenuNovoProduto.java
@@ -4,49 +4,77 @@ import com.joao.domain.FuncoesProdutos;
 
 import java.io.IOException;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class MenuNovoProduto {
 
     String name, description;
     float price;
-    int amount, flag;
+    int amount;
 
 
     public void infoRegistro() throws IOException {
-        Scanner scannerSTR = new Scanner(System.in);
-        Scanner scannerNUM = new Scanner(System.in);
-
         FuncoesProdutos fProdutos = new FuncoesProdutos();
 
         //Colhendo os dados do produto
         System.out.println("\n===== REGISTRANDO PRODUTOS =====");
 
-        System.out.print(" - Digite o nome do produto: ");
-        name = scannerSTR.nextLine();
+        int flag = 1;
+        int flag_erro = 1;
+        while (flag == 1) {
+            Scanner scannerSTR = new Scanner(System.in);
+            Scanner scannerNUM = new Scanner(System.in);
 
-        System.out.print(" - Informe a descrição do produto: ");
-        description = scannerSTR.nextLine();
+            try {
+                System.out.print(" - Digite o nome do produto: ");
+                name = scannerSTR.nextLine();
 
-        if (!fProdutos.validarProduto(name, description)) {
-            System.out.println("\nNome ou desccrição já existente");
-            System.out.println("Reinicie o programa e tente novamente");
-        }
+                System.out.print(" - Informe a descrição do produto: ");
+                description = scannerSTR.nextLine();
 
-        else {
-            System.out.print(" - Quantidade desse produto: ");
-            amount = scannerNUM.nextInt();
+                if (!fProdutos.validarProduto(name, description)) {
+                    System.out.println("\nNome ou desccrição já existente");
+                    System.out.println("Reinicie o programa e tente novamente");
+                    flag = 2;
+                }
 
-            System.out.print(" - Informe o preço do produto: R$");
-            price = scannerNUM.nextFloat();
+                else {
+                    System.out.print(" - Quantidade desse produto: ");
+                    amount = scannerNUM.nextInt();
 
-            if (fProdutos.validarAmount(amount)) {
-                System.out.println("\nQuantidade do produto não pode ser inferior a uma unidade");
-                System.out.println("Reinicie o programa e tente novamente");
+                    flag_erro = 2;
+                    System.out.print(" - Informe o preço do produto: R$");
+                    price = scannerNUM.nextFloat();
+
+                    if (fProdutos.validarAmount(amount)) {
+                        System.out.println("\nQuantidade do produto não pode ser inferior a uma unidade");
+                        System.out.println("Reinicie o programa e tente novamente");
+                        flag = 2;
+                    }
+
+                    else {
+                        fProdutos.criarProduto(name, description, price, amount);
+                        System.out.println("\n\u001B[32m !!! PRODUTO CADASTRADO COM SUCESSO !!! \u001B[m");
+                        flag = 2;
+                    }
+                }
             }
+            catch (InputMismatchException inputMismatchException) {
 
-            else {
-                fProdutos.criarProduto(name, description, price, amount);
-                System.out.println("\n\u001B[32m !!! PRODUTO CADASTRADO COM SUCESSO !!! \u001B[m");
+                if (flag_erro == 1) {
+                    System.out.println("-------------------------------------------------------");
+                    System.out.println("\u001B[31m                     !!! ERRO !!!  \u001B[m");
+                    System.out.println("Valor digitado fora dos parâmetros de um número inteiro");
+                    System.out.println("           Tente novamente (ex: 1, 2, 3...)");
+                    System.out.println("-------------------------------------------------------\n");
+                }
+                else {
+                    System.out.println("----------------------------------------------------");
+                    System.out.println("\u001B[31m                     !!! ERRO !!!  \u001B[m");
+                    System.out.println("Valor digitado fora dos parâmetros de um número real");
+                    System.out.println("          Tente novamente (ex: 1,2; 7,3...)");
+                    System.out.println("----------------------------------------------------\n");
+                }
             }
         }
     }
