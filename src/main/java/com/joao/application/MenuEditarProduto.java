ec5413ba60d8ae527390a9f4ca7f1385dd5c253a
JoaoFelipeSantana <j155c137@gmail.com>
2023-10-16 21:23:35 -0300

    [feat] - Adicionando mensagens de erro caso o usuário digite valores fora dos padrões de números reais ou inteiros

--- src/main/java/com/joao/application/MenuEditarProduto.java
+++ src/main/java/com/joao/application/MenuEditarProduto.java
@@ -5,73 +5,103 @@ import com.joao.domain.FuncoesProdutos;
 import com.google.gson.JsonObject;
 
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MenuEditarProduto {
 
 
     public void editarProduto() throws IOException {
-        Scanner scannerNUM = new Scanner(System.in);
-        Scanner scannerSTR = new Scanner (System.in);
         FuncoesProdutos fProdutos = new FuncoesProdutos();
 
         System.out.println("\n\n===== EDITAR PRODUTO =====");
 
-        System.out.print("Insira o id do produto a ser editado: ");
-        int id = scannerNUM.nextInt();
+        int flag = 1;
+        int flag_erro = 1;
 
-        JsonObject produto = fProdutos.consultarProduto(id);
+        while (flag == 1) {
+            Scanner scannerNUM = new Scanner(System.in);
+            Scanner scannerSTR = new Scanner (System.in);
 
-        if (!produto.isEmpty()){
-            System.out.println("\n===== INFORMAÇÕES ANTIGAS =====");
-            System.out.println(" - Nome: " + produto.get("nome"));
-            System.out.println(" - Descrição: " + produto.get("descricao"));
-            System.out.println(" - Preço: " + produto.get("preco"));
-            System.out.println(" - Quantidade: " + produto.get("quantidade"));
-            System.out.println(" - Data de criação: " + produto.get("dtcreate"));
-            System.out.println(" - Data de edição: " + produto.get("dtupdate"));
-            System.out.println("==============================");
+            try {
+                System.out.print("Insira o id do produto a ser editado: ");
+                int id = scannerNUM.nextInt();
 
+                JsonObject produto = fProdutos.consultarProduto(id);
 
-            System.out.println("\n===== INFORMAÇÕES ATUALIZADAS =====");
-
-            System.out.print(" - Nome: ");
-            String newName = scannerSTR.nextLine();
-
-            System.out.print(" - Descrição: ");
-            String newDescription = scannerSTR.nextLine();
-
-            if (!fProdutos.validarProduto(newName, newDescription)) {
-                System.out.println("\nNome ou desccrição já existente");
-                System.out.println("Reinicie o programa e tente novamente");
-            }
-
-            else {
-                System.out.print(" - Preço: ");
-                float newPrice = scannerNUM.nextFloat();
-
-                System.out.print(" - Quantidade: ");
-                int newAmount = scannerNUM.nextInt();
+                if (!produto.isEmpty()){
+                    System.out.println("\n===== INFORMAÇÕES ANTIGAS =====");
+                    System.out.println(" - Nome: " + produto.get("nome"));
+                    System.out.println(" - Descrição: " + produto.get("descricao"));
+                    System.out.println(" - Preço: " + produto.get("preco"));
+                    System.out.println(" - Quantidade: " + produto.get("quantidade"));
+                    System.out.println(" - Data de criação: " + produto.get("dtcreate"));
+                    System.out.println(" - Data de edição: " + produto.get("dtupdate"));
+                    System.out.println("==============================");
 
 
-                if (fProdutos.validarAmount(newAmount)) {
-                    System.out.println("\nQuantidade do produto não pode ser inferior a uma unidade");
-                    System.out.println("Reinicie o programa e tente novamente");
+                    System.out.println("\n===== INFORMAÇÕES ATUALIZADAS =====");
+
+                    System.out.print(" - Nome: ");
+                    String newName = scannerSTR.nextLine();
+
+                    System.out.print(" - Descrição: ");
+                    String newDescription = scannerSTR.nextLine();
+
+                    if (!fProdutos.validarProduto(newName, newDescription)) {
+                        System.out.println("\nNome ou desccrição já existente");
+                        System.out.println("Reinicie o programa e tente novamente");
+                        flag = 2;
+                    }
+
+                    else {
+                        System.out.print(" - Quantidade: ");
+                        int newAmount = scannerNUM.nextInt();
+
+                        flag_erro = 2;
+                        System.out.print(" - Preço: ");
+                        float newPrice = scannerNUM.nextFloat();
+
+                        if (fProdutos.validarAmount(newAmount)) {
+                            System.out.println("\nQuantidade do produto não pode ser inferior a uma unidade");
+                            System.out.println("Reinicie o programa e tente novamente");
+                            flag = 2;
+                        }
+
+                        else {
+                            fProdutos.editarProduto(produto.get("id").getAsInt(), newName, newDescription, newPrice, newAmount, produto.get("dtcreate").getAsString());
+                            System.out.println("\u001B[32m!!! PRODUTO ATUALIZADO COM SUCESSO !!!\u001B[32m");
+                            flag = 2;
+                        }
+                    }
                 }
 
                 else {
+                    flag = 2;
                     fProdutos.delete(id);
-                    fProdutos.editarProduto(produto.get("id").getAsInt(), newName, newDescription, newPrice, newAmount, produto.get("dtcreate").getAsString());
-                    System.out.println("\u001B[32m!!! PRODUTO ATUALIZADO COM SUCESSO !!!\u001B[32m");
+                    System.out.println("\n Produto não encontrado");
+                    System.out.println("Reinicie o programa e tente novamente...");
+                }
+            }
+
+            catch (InputMismatchException inputMismatchException) {
+
+                if (flag_erro == 1) {
+                    System.out.println("-------------------------------------------------------");
+                    System.out.println("\u001B[31m                     !!! ERRO !!!  \u001B[m");
+                    System.out.println("Valor digitado fora dos parâmetros de um número inteiro");
+                    System.out.println("           Tente novamente (ex: 1, 2, 3...)");
+                    System.out.println("-------------------------------------------------------\n");
+                }
+                else {
+                    System.out.println("----------------------------------------------------");
+                    System.out.println("\u001B[31m                     !!! ERRO !!!  \u001B[m");
+                    System.out.println("Valor digitado fora dos parâmetros de um número real");
+                    System.out.println("          Tente novamente (ex: 1,2; 7,3...)");
+                    System.out.println("----------------------------------------------------\n");
                 }
 
             }
-
-        }
-
-        else {
-            System.out.println("\n Produto não encontrado");
-            System.out.println("Reinicie o programa e tente novamente...");
         }
     }
 }
