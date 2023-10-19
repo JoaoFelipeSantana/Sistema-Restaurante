5a73cf2e39dd4968da219ce5300b8b2f9d4bc366
JoaoFelipeSantana <j155c137@gmail.com>
2023-10-18 21:03:29 -0300

    [feat] - Adicionando os métodos acessores para a entidade Product

--- src/main/java/com/joao/infrastructure/entity/Product.java
+++ src/main/java/com/joao/infrastructure/entity/Product.java
@@ -11,12 +11,41 @@ public class Product {
     private String dtcreate;
 
     // == Contrutor para setar os valores da entidade ==
-    public Product(int id, String name, String description, float price, int amount, String dtupdate, String dtcreat) {
+    public Product(int id, String name, String description, float price, int amount,  String dtcreat, String dtupdate) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.price = price;
         this.amount = amount;
+        this.dtcreate = dtcreat;
         this.dtupdate = dtupdate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public String getDtupdate() {
+        return dtupdate;
+    }
+
+    public String getDtcreate() {
+        return dtcreate;
     }
 }
